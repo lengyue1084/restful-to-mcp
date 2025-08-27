b2b7fb96b06c92539badc03eab8e520fd0dba4d0
lengyue1084 <1084287358@qq.com>
2025-08-27 14:47:15 +0800

    refactor(internal/data/database): 重构数据库事务处理方法
    - 移除了 ExecTx 方法，简化事务处理逻辑
    - 新增 GetDb 方法，用于获取上下文中的事务对象
    - 优化了事务管理，避免多表操作时出现不同的 tx 对象

--- internal/data/database/db.go
+++ internal/data/database/db.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"context"
+	"flow-bridge-mcp/internal/data/model"
 	"flow-bridge-mcp/pkg/logger"
+	"fmt"
 	"github.com/go-redis/redis/v8"
 	"gorm.io/gorm"
 )
@@ -20,16 +22,10 @@ func NewData(db *gorm.DB, redis *redis.Client, log *logger.Logger) (*Data, func(
 	return &Data{Db: db, redis: redis}, cleanup, nil
 }
 
-func (d *Data) ExecTx(ctx context.Context, fn func(ctx context.Context) error) (err error) {
-
-	err = d.Db.Transaction(func(tx *gorm.DB) error {
-		// 将事务对象放入 context
-		ctx = context.WithValue(ctx, "tx", tx)
-		// 执行业务逻辑
-		return fn(ctx)
-		// 如果 fn 返回 error，GORM 自动回滚
-		// 如果 fn 返回 nil，GORM 自动提交
-	})
-
-	return err
+// GetDb biz 层开启事务可以使用该方法获取同一个tx对象，避免多表操作时，tx对象不一致
+func (m *Data) GetDb(ctx context.Context) (db *gorm.DB, err error) {
+	if tx, ok := ctx.Value(model.TxKey).(*gorm.DB); ok {
+		return tx, nil
+	}
+	return nil, fmt.Errorf("tx is not exist")
 }
