db97bd3ab5ff5a8f781611eafea8454efe67befe
lengyue1084 <1084287358@qq.com>
2025-09-03 14:44:36 +0800

    feat(const): 添加全局常量和状态类型
    - 新增多个状态类型和常量，用于认证、工具存在状态、服务器类型等- 添加通用的显示和隐藏状态常量
    - 定义服务器 token 状态常量和刷新时间常量
    - 实现各个状态类型的 String 方法，用于友好的字符串表示
    - 添加上下文键和文件不存在错误常量

---
+++ pkg/const/const.go
@@ -0,0 +1,103 @@
+package _const
+
+import "errors"
+
+type (
+	AuthStatus          int8
+	HaveToolsStatus     int8
+	McpServerTypeStatus int8
+	Status              int8
+	ServerStatus        int8
+)
+
+const (
+	IsAuthNo             AuthStatus          = 1
+	IsAuthYes            AuthStatus          = 2
+	HaveToolsNo          HaveToolsStatus     = 1
+	HaveToolsYes         HaveToolsStatus     = 2
+	McpServerTypeOpenapi McpServerTypeStatus = 1
+	McpServerTypeGrpc    McpServerTypeStatus = 2
+
+	// StatusDisplay 通用的是否显示
+	StatusDisplay Status = 1
+	StatusHidden  Status = 2
+
+	ServerNotSetToken    ServerStatus = 1
+	ServerHadSetToken    ServerStatus = 2
+	ServerTokenIsWorking ServerStatus = 3
+)
+
+const (
+	McpServerRefreshTicketTime = 60 * 10
+	CommonContextTimeOut       = 5
+)
+
+func (s ServerStatus) Sting() string {
+	switch s {
+	case ServerNotSetToken:
+		return "未设置"
+	case ServerHadSetToken:
+		return "已设置"
+	case ServerTokenIsWorking:
+		return "正常"
+	default:
+		return "未知"
+	}
+
+}
+
+func (s Status) String() string {
+
+	switch s {
+	case StatusDisplay:
+		return "显示"
+	case StatusHidden:
+		return "隐藏"
+	default:
+		return "未知"
+
+	}
+}
+
+func (a AuthStatus) String() string {
+	switch a {
+	case IsAuthNo:
+		return "否"
+	case IsAuthYes:
+		return "是"
+	default:
+		return "未知"
+	}
+}
+func (h HaveToolsStatus) String() string {
+	switch h {
+	case HaveToolsNo:
+		return "否"
+	case HaveToolsYes:
+		return "是"
+	default:
+		return "未知"
+	}
+
+}
+
+func (m McpServerTypeStatus) String() string {
+	switch m {
+	case McpServerTypeOpenapi:
+		return "OpenAPI"
+	case McpServerTypeGrpc:
+		return "gRPC"
+	default:
+		return "未知"
+	}
+}
+
+type contextKey string
+
+const (
+	TxKey contextKey = "tx"
+)
+
+var (
+	McpFileIsNotExist = errors.New("mcp file is not exist")
+)
