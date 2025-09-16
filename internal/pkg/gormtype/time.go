89ab255f5eb27cd12fe291ff0cd57e88676fbd39
lengyue1084 <1084287358@qq.com>
2025-09-16 17:03:00 +0800

    refactor(internal/pkg/gormtype): 优化 LocalTime 类型的 Value 方法
    - 将 LocalTime 类型由值接收者改为指针接收者，减少不必要的复制
    - 优化时间转换逻辑，提高代码可读性和性能

--- internal/pkg/gormtype/time.go
+++ internal/pkg/gormtype/time.go
@@ -18,9 +18,9 @@ func (t *LocalTime) MarshalJSONDate() ([]byte, error) {
 	return []byte(fmt.Sprintf("\"%v\"", tTime.Format("2006-01-02"))), nil
 }
 
-func (t LocalTime) Value() (driver.Value, error) {
+func (t *LocalTime) Value() (driver.Value, error) {
 	var zeroTime time.Time
-	tlt := time.Time(t)
+	tlt := time.Time(*t)
 	//判断给定时间是否和默认零时间的时间戳相同
 	if tlt.UnixNano() == zeroTime.UnixNano() {
 		return nil, nil
